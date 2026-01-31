e25180bf8049150d821ce16b76e168b7a5c8e111
ahmed <ahmedla9lou9@gmail.com>
2026-01-31 04:11:38 +0100

    add contact and fix navbar

--- src/app/contact/components/contact-form/contact-form.ts
+++ src/app/contact/components/contact-form/contact-form.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-form.html',
-  styleUrl: './contact-form.css',
+  styleUrl: './contact-form.css'
 })
 export class ContactForm {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formulaire envoyé !', this.contactForm.value);
+    }
+  }
 }
--- src/app/contact/components/contact-info/contact-info.ts
+++ src/app/contact/components/contact-info/contact-info.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-info',
+  standalone: true,
   imports: [],
   templateUrl: './contact-info.html',
   styleUrl: './contact-info.css',
